1b731a6b5cbcb3700a157c7980b0a9835b67207d
dlantjdgkgk <antjdgk@gmail.com>
2025-06-22 02:29:52 +0900

    Implement code changes to enhance functionality and improve performance

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,13 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-	addons: ['@chromatic-com/storybook'],
+	addons: [
+		'@storybook/addon-onboarding',
+		'@storybook/addon-essentials',
+		'@chromatic-com/storybook',
+		'@storybook/addon-interactions',
+		'@storybook/addon-themes',
+	],
 	framework: {
 		name: '@storybook/react-vite',
 		options: {},
