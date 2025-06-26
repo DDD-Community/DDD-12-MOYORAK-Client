7de1b8fd2106177393bb82447871bd81b14769dc
dlantjdgkgk <antjdgk@gmail.com>
2025-06-26 22:05:28 +0900

    refactor : 코드 수정

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,23 +1,19 @@
 import Icon, { type IconTypes } from '@/components/Icon';
 
-interface BaseNavBarProps {
-	variant: 'iconOnly' | 'iconWithText' | 'iconWithTextAndRightIcon' | 'centerText';
-}
-
-interface IconOnlyProps extends BaseNavBarProps {
+interface IconOnlyProps {
 	variant: 'iconOnly';
 	leftIcon?: IconTypes;
 	onLeftIconClick?: () => void;
 }
 
-interface IconWithTextProps extends BaseNavBarProps {
+interface IconWithTextProps {
 	variant: 'iconWithText';
 	leftIcon?: IconTypes;
 	leftText: string;
 	onLeftIconClick?: () => void;
 }
 
-interface IconWithTextAndRightIconProps extends BaseNavBarProps {
+interface IconWithTextAndRightIconProps {
 	variant: 'iconWithTextAndRightIcon';
 	leftIcon?: IconTypes;
 	leftText: string;
@@ -26,7 +22,7 @@ interface IconWithTextAndRightIconProps extends BaseNavBarProps {
 	onRightIconClick?: () => void;
 }
 
-interface CenterTextProps extends BaseNavBarProps {
+interface CenterTextProps {
 	variant: 'centerText';
 	centerText: string;
 }
@@ -34,6 +30,8 @@ interface CenterTextProps extends BaseNavBarProps {
 type INavBarProps = IconOnlyProps | IconWithTextProps | IconWithTextAndRightIconProps | CenterTextProps;
 
 const NavBar = (props: INavBarProps) => {
+	const textStyle = 'text-[#121212] text-lg font-semibold leading-[144.5%] tracking-[-0.004px]';
+
 	const renderContent = () => {
 		switch (props.variant) {
 			case 'iconOnly':
@@ -47,7 +45,7 @@ const NavBar = (props: INavBarProps) => {
 				return (
 					<div className="flex items-center gap-4">
 						<Icon name={props.leftIcon || 'back'} size={24} onClick={props.onLeftIconClick} />
-						<span className="text-[#121212] text-lg font-semibold leading-[144.5%] tracking-[-0.004px]">{props.leftText}</span>
+						<span className={textStyle}>{props.leftText}</span>
 					</div>
 				);
 
@@ -56,7 +54,7 @@ const NavBar = (props: INavBarProps) => {
 					<div className="flex items-center justify-between w-full">
 						<div className="flex items-center gap-4">
 							<Icon name={props.leftIcon || 'back'} size={24} onClick={props.onLeftIconClick} />
-							<span className="text-[#121212] text-lg font-semibold leading-[144.5%] tracking-[-0.004px]">{props.leftText}</span>
+							<span className={textStyle}>{props.leftText}</span>
 						</div>
 						<Icon name={props.rightIcon} size={24} onClick={props.onRightIconClick} />
 					</div>
@@ -65,7 +63,7 @@ const NavBar = (props: INavBarProps) => {
 			case 'centerText':
 				return (
 					<div className="flex items-center justify-center w-full">
-						<span className="text-[#121212] text-lg font-semibold leading-[144.5%] tracking-[-0.004px]">{props.centerText}</span>
+						<span className={textStyle}>{props.centerText}</span>
 					</div>
 				);
 
@@ -78,30 +76,3 @@ const NavBar = (props: INavBarProps) => {
 };
 
 export default NavBar;
-
-// const NavBarShowcase = () => {
-// 	return (
-// 		<div className="space-y-4 bg-gray-50 min-h-screen">
-// 			{/* Case 1: 왼쪽 아이콘만 */}
-// 			<NavBar variant="iconOnly" leftIcon="back" onLeftIconClick={() => console.log('Back clicked')} />
-
-// 			{/* Case 2: 왼쪽 아이콘 + 왼쪽 글씨 */}
-// 			<NavBar variant="iconWithText" leftIcon="back" leftText="제목" onLeftIconClick={() => console.log('Back clicked')} />
-
-// 			{/* Case 3: 왼쪽 아이콘 + 왼쪽 글씨 + 오른쪽 아이콘 */}
-// 			<NavBar
-// 				variant="iconWithTextAndRightIcon"
-// 				leftIcon="back"
-// 				leftText="제목"
-// 				rightIcon="close"
-// 				onLeftIconClick={() => console.log('Back clicked')}
-// 				onRightIconClick={() => console.log('Close clicked')}
-// 			/>
-
-// 			{/* Case 4: 가운데 글씨 */}
-// 			<NavBar variant="centerText" centerText="제목" />
-// 		</div>
-// 	);
-// };
-
-// export default NavBarShowcase;
