cb8912f11195739fcc3fe2fa3bf8ee2bb1f8e284
dlantjdgkgk <antjdgk@gmail.com>
2025-07-04 00:34:26 +0900

    fix : Typography className 적용 안되는 이슈 해결

--- src/components/TabBar/TabItem.tsx
+++ src/components/TabBar/TabItem.tsx
@@ -17,7 +17,7 @@ const TabItem = ({ path, iconName, activeIconName, label, currentPath, onClick }
 		<button onClick={() => onClick(path)}>
 			<div className="flex flex-col gap-[6px] items-center">
 				<Icon name={isActive ? activeIconName : iconName} />
-				<Typography as="span" variant={FONT_VARIANT.caption02} fontColor={isActive ? PALETTE.primary200 : PALETTE.gray07}>
+				<Typography as="span" variant={FONT_VARIANT.caption02} fontColor={isActive ? PALETTE.primary200 : PALETTE.gray07} className="font-medium">
 					{label}
 				</Typography>
 			</div>
