8e2f9cf75289d3c4718b220708c0b5681e1fa3bb
sypaik <sypaik.dev@gmail.com>
2025-07-25 02:25:46 +0900

    feat: auth / redirect 페이지 추가 (#35)

---
+++ src/pages/AuthPage/AuthPage.tsx
@@ -0,0 +1,57 @@
+import auth from '@/assets/auth.png';
+import Icon from '@/components/Icon';
+import NavBar from '@/components/NavBar/NavBar';
+import Typography from '@/components/Typography';
+import { FONT_VARIANT, PALETTE } from '@/constants/styles';
+
+const AuthPage = () => {
+	const googleLogin = () => {
+		const CLIENT_ID = import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID as string;
+		const REDIRECT_URI = import.meta.env.VITE_GOOGLE_AUTH_REDIRECT_URI as string;
+
+		const params = new URLSearchParams({
+			client_id: CLIENT_ID,
+			redirect_uri: REDIRECT_URI,
+			response_type: 'code',
+			scope: 'email profile',
+		});
+
+		const link = `https://accounts.google.com/o/oauth2/v2/auth?${params.toString()}`;
+		window.location.href = link;
+	};
+
+	return (
+		<main>
+			<header>
+				<NavBar variant="iconOnly" />
+			</header>
+
+			<section className="px-5 mb-[89px]">
+				<Typography as="h1" variant={FONT_VARIANT.title02} className="mt-[65px] mb-[13px] font-bold">
+					모여락에
+					<br />
+					오신 걸 환영합니다!
+				</Typography>
+				<Typography as="h2" variant={FONT_VARIANT.body01} fontColor={PALETTE.gray07}>
+					모여락은 팀원들과 함께
+					<br />
+					맛집 공유 리스트를 만들고 관리하는 서비스로
+					<br />
+					로그인 후 다양한 서비스를 이용할 수 있습니다.
+				</Typography>
+			</section>
+
+			<img src={auth} className="mx-auto mb-[104px]" />
+			<button onClick={googleLogin} className="border-[1px] border-gray-05 flex items-center gap-[11.9px] py-[13px] px-[72px] rounded-[50px] mb-[15px] mx-auto">
+				<Icon name="googleLogin" />
+				<Typography variant={FONT_VARIANT.body01}>Google로 간편하게 시작</Typography>
+			</button>
+
+			<Typography variant={FONT_VARIANT.caption01} className="text-center">
+				모여락은 현재 구글 간편 로그인으로만 이용할 수 있습니다.
+			</Typography>
+		</main>
+	);
+};
+
+export default AuthPage;
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import TabBar from '@/components/TabBar/TabBar';
+import AuthPage from '@/pages/AuthPage/AuthPage';
+import RedirectPage from '@/pages/AuthPage/RedirectPage';
 import Developer from '@/pages/Developer';
 import Main from '@/pages/Main';
 import MyPage from '@/pages/MyPage';
@@ -16,6 +18,8 @@ const AppRouter = () => {
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Main />} />
+				<Route path="/auth" element={<AuthPage />} />
+				<Route path="/redirect" element={<RedirectPage />} />
 				<Route path="/search" element={<TeamShareListSearch />} />
 				<Route path="/mypage" element={<MyPage />} />
 				<Route path="/pot" element={<Pot />} />
