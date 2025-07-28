45a7033127f8f7722c413b68dcbef399354a8009
sypaik <sypaik.dev@gmail.com>
2025-07-28 22:59:52 +0900

    chore: api url 환경변수 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
 	readonly VITE_KAKAO_MAP_KEY: string;
 	readonly VITE_GOOGLE_AUTH_CLIENT_ID: string;
 	readonly VITE_GOOGLE_AUTH_REDIRECT_URI: string;
+	readonly VITE_API_URL: string;
 }
 
 declare global {
