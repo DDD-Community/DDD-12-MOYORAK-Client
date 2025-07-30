3a02b016616a41255c0a344fd8fa9dbb3a01c883
sypaik <sypaik.dev@gmail.com>
2025-07-31 07:48:40 +0900

    fix: merge conflict 해소

--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import { useNavigate, useSearchParams } from 'react-router-dom';
 
 import NavBar from '@/components/NavBar/NavBar';
 import Typography from '@/components/Typography';
